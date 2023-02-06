8440e99b72a62fc2239ca257af13f868fed40d07
yekta <ngyekta@gmail.com>
2023-02-06 14:09:54 +0300

    Jpeg default

--- shared/constants.go
+++ shared/constants.go
@@ -33,7 +33,7 @@ const (
 )
 
 // Default image extension for generate
-const DEFAULT_GENERATE_OUTPUT_EXTENSION = WEBP
+const DEFAULT_GENERATE_OUTPUT_EXTENSION = JPEG
 
 // Allowed image extensions for upload
 var ALLOWS_IMAGE_EXTENSIONS_UPLOAD = []ImageExtension{WEBP, JPEG, PNG}
