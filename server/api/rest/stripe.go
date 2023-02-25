6de9063a9332eb60e33468c04f73a81ee4be5715
Brandon Berhent <bbedward@gmail.com>
2023-02-25 20:55:20 +0000

    Handle new subscription creation

--- server/api/rest/stripe.go
+++ server/api/rest/stripe.go
@@ -8,6 +8,7 @@ import (
 	"github.com/go-chi/render"
 	"github.com/stablecog/sc-go/server/responses"
 	"github.com/stablecog/sc-go/utils"
+	"github.com/stripe/stripe-go"
 	"github.com/stripe/stripe-go/webhook"
 	"k8s.io/klog/v2"
 )
@@ -32,93 +33,121 @@ func (c *RestAPI) HandleStripeWebhook(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// We can parse the object as an invoice since that's the only thing we care about
-	invoice, err := stripeObjectMapToInvoiceObject(event.Data.Object)
-	if err != nil || invoice == nil {
-		klog.Errorf("Unable parsing stripe invoice object: %v", err)
-		responses.ErrInternalServerError(w, r, err.Error())
-		return
-	}
-
-	// We only care about renewal (cycle), create, and manual
-	if invoice.BillingReason != InvoiceBillingReasonSubscriptionCycle && invoice.BillingReason != InvoiceBillingReasonSubscriptionCreate && invoice.BillingReason != InvoiceBillingReasonManual {
-		render.Status(r, http.StatusOK)
-		render.PlainText(w, r, "OK")
-		return
-	}
-
-	if invoice.Lines == nil {
-		klog.Errorf("Stripe invoice lines is nil %s", invoice.ID)
-		responses.ErrInternalServerError(w, r, "Stripe invoice lines is nil")
-		return
-	}
-
-	for _, line := range invoice.Lines.Data {
-		var product string
-		if line.Plan == nil && invoice.BillingReason != InvoiceBillingReasonManual {
-			klog.Errorf("Stripe plan is nil in line item %s", line.ID)
-			responses.ErrInternalServerError(w, r, "Stripe plan is nil in line item")
-			return
-		}
-
-		if line.Price == nil && invoice.BillingReason == InvoiceBillingReasonManual {
-			klog.Errorf("Stripe price is nil in line item %s", line.ID)
-			responses.ErrInternalServerError(w, r, "Stripe price is nil in line item")
-			return
-		}
-
-		if invoice.BillingReason == InvoiceBillingReasonManual {
-			product = line.Price.Product
-		} else {
-			product = line.Plan.Product
-		}
-
-		if product == "" {
-			klog.Errorf("Stripe product is nil in line item %s", line.ID)
-			responses.ErrInternalServerError(w, r, "Stripe product is nil in line item")
-			return
-		}
-
-		// Get user from customer ID
-		user, err := c.Repo.GetUserByStripeCustomerId(invoice.Customer)
-		if err != nil {
-			klog.Errorf("Unable getting user from stripe customer id: %v", err)
+	switch event.Type {
+	case "customer.subscription.created":
+		newSub, err := stripeObjectMapToSubscriptionObject(event.Data.Object)
+		if err != nil || newSub == nil {
+			klog.Errorf("Unable parsing stripe subscription object: %v", err)
 			responses.ErrInternalServerError(w, r, err.Error())
 			return
-		} else if user == nil {
-			klog.Errorf("User does not exist with stripe customer id: %s", invoice.Customer)
-			responses.ErrInternalServerError(w, r, "User does not exist with stripe customer id")
-			return
 		}
-
-		// Get the credit type for this plan
-		creditType, err := c.Repo.GetCreditTypeByStripeProductID(product)
-		if err != nil {
-			klog.Errorf("Unable getting credit type from stripe product id: %v", err)
+		// We need to see if they have more than one subscription
+		subIter := c.StripeClient.Subscriptions.List(&stripe.SubscriptionListParams{
+			Customer: newSub.Customer.ID,
+		})
+		for subIter.Next() {
+			sub := subIter.Subscription()
+			if sub.ID != newSub.ID {
+				// We need to cancel this subscription
+				_, err := c.StripeClient.Subscriptions.Update(sub.ID, &stripe.SubscriptionParams{
+					CancelAtPeriodEnd: stripe.Bool(true),
+				})
+				if err != nil {
+					klog.Errorf("Unable canceling stripe subscription: %v", err)
+					responses.ErrInternalServerError(w, r, err.Error())
+					return
+				}
+			}
+		}
+	case "invoice.payment_succeeded":
+		// We can parse the object as an invoice since that's the only thing we care about
+		invoice, err := stripeObjectMapToInvoiceObject(event.Data.Object)
+		if err != nil || invoice == nil {
+			klog.Errorf("Unable parsing stripe invoice object: %v", err)
 			responses.ErrInternalServerError(w, r, err.Error())
 			return
-		} else if creditType == nil {
-			klog.Errorf("Credit type does not exist with stripe product id: %s", line.Plan.Product)
-			responses.ErrInternalServerError(w, r, "Credit type does not exist with stripe product id")
+		}
+
+		// We only care about renewal (cycle), create, and manual
+		if invoice.BillingReason != InvoiceBillingReasonSubscriptionCycle && invoice.BillingReason != InvoiceBillingReasonSubscriptionCreate && invoice.BillingReason != InvoiceBillingReasonManual {
+			render.Status(r, http.StatusOK)
+			render.PlainText(w, r, "OK")
 			return
 		}
 
-		if invoice.BillingReason == InvoiceBillingReasonManual {
-			// Ad-hoc credit add
-			_, err = c.Repo.AddAdhocCreditsIfEligible(creditType, user.ID, line.ID)
-			if err != nil {
-				klog.Errorf("Unable adding credits to user %s: %v", user.ID.String(), err)
-				responses.ErrInternalServerError(w, r, err.Error())
+		if invoice.Lines == nil {
+			klog.Errorf("Stripe invoice lines is nil %s", invoice.ID)
+			responses.ErrInternalServerError(w, r, "Stripe invoice lines is nil")
+			return
+		}
+
+		for _, line := range invoice.Lines.Data {
+			var product string
+			if line.Plan == nil && invoice.BillingReason != InvoiceBillingReasonManual {
+				klog.Errorf("Stripe plan is nil in line item %s", line.ID)
+				responses.ErrInternalServerError(w, r, "Stripe plan is nil in line item")
 				return
 			}
-		} else {
-			expiresAt := utils.SecondsSinceEpochToTime(line.Period.End)
-			// Update user credit
-			_, err = c.Repo.AddCreditsIfEligible(creditType, user.ID, expiresAt, line.ID, nil)
+
+			if line.Price == nil && invoice.BillingReason == InvoiceBillingReasonManual {
+				klog.Errorf("Stripe price is nil in line item %s", line.ID)
+				responses.ErrInternalServerError(w, r, "Stripe price is nil in line item")
+				return
+			}
+
+			if invoice.BillingReason == InvoiceBillingReasonManual {
+				product = line.Price.Product
+			} else {
+				product = line.Plan.Product
+			}
+
+			if product == "" {
+				klog.Errorf("Stripe product is nil in line item %s", line.ID)
+				responses.ErrInternalServerError(w, r, "Stripe product is nil in line item")
+				return
+			}
+
+			// Get user from customer ID
+			user, err := c.Repo.GetUserByStripeCustomerId(invoice.Customer)
 			if err != nil {
-				klog.Errorf("Unable adding credits to user %s: %v", user.ID.String(), err)
+				klog.Errorf("Unable getting user from stripe customer id: %v", err)
 				responses.ErrInternalServerError(w, r, err.Error())
 				return
+			} else if user == nil {
+				klog.Errorf("User does not exist with stripe customer id: %s", invoice.Customer)
+				responses.ErrInternalServerError(w, r, "User does not exist with stripe customer id")
+				return
+			}
+
+			// Get the credit type for this plan
+			creditType, err := c.Repo.GetCreditTypeByStripeProductID(product)
+			if err != nil {
+				klog.Errorf("Unable getting credit type from stripe product id: %v", err)
+				responses.ErrInternalServerError(w, r, err.Error())
+				return
+			} else if creditType == nil {
+				klog.Errorf("Credit type does not exist with stripe product id: %s", line.Plan.Product)
+				responses.ErrInternalServerError(w, r, "Credit type does not exist with stripe product id")
+				return
+			}
+
+			if invoice.BillingReason == InvoiceBillingReasonManual {
+				// Ad-hoc credit add
+				_, err = c.Repo.AddAdhocCreditsIfEligible(creditType, user.ID, line.ID)
+				if err != nil {
+					klog.Errorf("Unable adding credits to user %s: %v", user.ID.String(), err)
+					responses.ErrInternalServerError(w, r, err.Error())
+					return
+				}
+			} else {
+				expiresAt := utils.SecondsSinceEpochToTime(line.Period.End)
+				// Update user credit
+				_, err = c.Repo.AddCreditsIfEligible(creditType, user.ID, expiresAt, line.ID, nil)
+				if err != nil {
+					klog.Errorf("Unable adding credits to user %s: %v", user.ID.String(), err)
+					responses.ErrInternalServerError(w, r, err.Error())
+					return
+				}
 			}
 		}
 	}
@@ -139,6 +168,20 @@ func stripeObjectMapToInvoiceObject(obj map[string]interface{}) (*Invoice, error
 		return nil, err
 	}
 	return &invoice, nil
+}
+
+// Parse generic object into stripe subscription struct
+func stripeObjectMapToSubscriptionObject(obj map[string]interface{}) (*stripe.Subscription, error) {
+	marshalled, err := json.Marshal(obj)
+	if err != nil {
+		return nil, err
+	}
+	var subscription stripe.Subscription
+	err = json.Unmarshal(marshalled, &subscription)
+	if err != nil {
+		return nil, err
+	}
+	return &subscription, nil
 }
 
 // ! Stripe types are busted so we modify the ones included in their lib
