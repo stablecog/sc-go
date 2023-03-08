c262621f3d61554cd2979fbc33ef1df74e4b433c
Brandon Berhent <bbedward@gmail.com>
2023-03-08 18:36:01 +0000

    Log queue throttling

--- server/api/rest/generate.go
+++ server/api/rest/generate.go
@@ -55,6 +55,8 @@ func (c *RestAPI) HandleCreateGeneration(w http.ResponseWriter, r *http.Request)
 	}
 
 	// Get queue count
+	log.Infof("Queue count: %d", c.QueueThrottler.NumQueued(user.ID.String()))
+	log.Infof("Queue max: %d", qMax)
 	if c.QueueThrottler.NumQueued(user.ID.String()) > qMax {
 		responses.ErrBadRequest(w, r, "queue_limit_reached")
 		return
