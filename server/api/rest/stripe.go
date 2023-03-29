3152a93ca719a6c69aecc11d5077e665a55b2fb5
Brandon Berhent <bbedward@gmail.com>
2023-03-29 19:29:19 +0000

    mega pack

--- server/api/rest/stripe.go
+++ server/api/rest/stripe.go
@@ -49,6 +49,7 @@ func GetSinglePurchasePriceIDs() map[string]string {
 	return map[string]string{
 		utils.GetEnv("STRIPE_LARGE_PACK_PRICE_ID", "1"):  utils.GetEnv("STRIPE_LARGE_PACK_PRODUCT_ID", "1"),
 		utils.GetEnv("STRIPE_MEDIUM_PACK_PRICE_ID", "2"): utils.GetEnv("STRIPE_MEDIUM_PACK_PRODUCT_ID", "2"),
+		utils.GetEnv("STRIPE_MEGA_PACK_PRICE_ID", "3"):   utils.GetEnv("STRIPE_MEGA_PACK_PRODUCT_ID", "3"),
 	}
 }
 
