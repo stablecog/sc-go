b731051411ed0d7013beb89709f70375d5ea77f2
Brandon Berhent <bbedward@gmail.com>
2023-04-01 17:30:44 +0000

    Disable distinct

--- server/api/rest/clipapi.go
+++ server/api/rest/clipapi.go
@@ -188,7 +188,7 @@ func (c *RestAPI) HandleClipSearchPGVector(w http.ResponseWriter, r *http.Reques
 
 	vector := clipAPIResponse.Embeddings[0].Embedding
 
-	res, err := c.Repo.DB.Debug().GenerationOutput.Query().Select(generationoutput.FieldImagePath).
+	res, err := c.Repo.DB.Debug().GenerationOutput.Query().Unique(false).Select(generationoutput.FieldImagePath).
 		WithGenerations(func(gq *ent.GenerationQuery) {
 			gq.WithPrompt()
 		}).
