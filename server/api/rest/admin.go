3249c3cc96dfbf0b604624756ca63ec5673a1d91
Brandon Berhent <bbedward@gmail.com>
2023-04-11 23:36:05 +0000

    Add search to admin generations

--- server/api/rest/admin.go
+++ server/api/rest/admin.go
@@ -10,9 +10,12 @@ import (
 	"time"
 
 	"github.com/go-chi/render"
+	"github.com/google/uuid"
 	"github.com/stablecog/sc-go/database/ent"
 	"github.com/stablecog/sc-go/database/ent/generationoutput"
 	"github.com/stablecog/sc-go/database/ent/userrole"
+	"github.com/stablecog/sc-go/database/qdrant"
+	"github.com/stablecog/sc-go/database/repository"
 	"github.com/stablecog/sc-go/log"
 	"github.com/stablecog/sc-go/server/requests"
 	"github.com/stablecog/sc-go/server/responses"
@@ -125,15 +128,8 @@ func (c *RestAPI) HandleQueryGenerationsForAdmin(w http.ResponseWriter, r *http.
 		}
 	}
 
-	var cursor *time.Time
-	if cursorStr := r.URL.Query().Get("cursor"); cursorStr != "" {
-		cursorTime, err := utils.ParseIsoTime(cursorStr)
-		if err != nil {
-			responses.ErrBadRequest(w, r, "cursor must be a valid iso time string or offset", "")
-			return
-		}
-		cursor = &cursorTime
-	}
+	cursorStr := r.URL.Query().Get("cursor")
+	search := r.URL.Query().Get("search")
 
 	filters := &requests.QueryGenerationFilters{}
 	err = filters.ParseURLQueryParameters(r.URL.Query())
@@ -141,6 +137,7 @@ func (c *RestAPI) HandleQueryGenerationsForAdmin(w http.ResponseWriter, r *http.
 		responses.ErrBadRequest(w, r, err.Error(), "")
 		return
 	}
+
 	// Make sure non-super admin can't get private generations
 	if !superAdmin {
 		if len(filters.GalleryStatus) == 0 {
@@ -153,6 +150,119 @@ func (c *RestAPI) HandleQueryGenerationsForAdmin(w http.ResponseWriter, r *http.
 			responses.ErrUnauthorized(w, r)
 			return
 		}
+	}
+
+	// For search, use qdrant semantic search
+	if search != "" {
+		// get embeddings from clip service
+		e, err := c.Clip.GetEmbeddingFromText(search, 2)
+		if err != nil {
+			log.Error("Error getting embedding from clip service", "err", err)
+			responses.ErrInternalServerError(w, r, "An unknown error has occured")
+			return
+		}
+
+		// Parse as qdrant filters
+		qdrantFilters, scoreThreshold := filters.ToQdrantFilters(false)
+		// Deleted at not empty
+		qdrantFilters.Must = append(qdrantFilters.Must, qdrant.SCMatchCondition{
+			IsEmpty: &qdrant.SCIsEmpty{Key: "deleted_at"},
+		})
+
+		// Get cursor str as uint
+		var offset *uint
+		var total *uint
+		if cursorStr != "" {
+			cursoru64, err := strconv.ParseUint(cursorStr, 10, 64)
+			if err != nil {
+				responses.ErrBadRequest(w, r, "cursor must be a valid uint", "")
+				return
+			}
+			cursoru := uint(cursoru64)
+			offset = &cursoru
+		} else {
+			count, err := c.Qdrant.CountWithFilters(qdrantFilters, false)
+			if err != nil {
+				log.Error("Error counting qdrant", "err", err)
+				responses.ErrInternalServerError(w, r, "An unknown error has occured")
+				return
+			}
+			total = &count
+		}
+
+		// Query qdrant
+		qdrantRes, err := c.Qdrant.QueryGenerations(e, perPage, offset, scoreThreshold, qdrantFilters, false, false)
+		if err != nil {
+			log.Error("Error querying qdrant", "err", err)
+			responses.ErrInternalServerError(w, r, "An unknown error has occured")
+			return
+		}
+
+		// Get generation output ids
+		var outputIds []uuid.UUID
+		for _, hit := range qdrantRes.Result {
+			outputId, err := uuid.Parse(hit.Id)
+			if err != nil {
+				log.Error("Error parsing uuid", "err", err)
+				continue
+			}
+			outputIds = append(outputIds, outputId)
+		}
+
+		// Get user generation data in correct format
+		generationsUnsorted, err := c.Repo.RetrieveGenerationsWithOutputIDs(outputIds)
+		if err != nil {
+			log.Error("Error getting generations", "err", err)
+			responses.ErrInternalServerError(w, r, "An unknown error has occured")
+			return
+		}
+
+		// Need to re-sort to preserve qdrant ordering
+		gDataMap := make(map[uuid.UUID]repository.GenerationQueryWithOutputsResultFormatted)
+		for _, gData := range generationsUnsorted.Outputs {
+			gDataMap[gData.ID] = gData
+		}
+
+		var generations []repository.GenerationQueryWithOutputsResultFormatted
+		for _, hit := range qdrantRes.Result {
+			outputId, err := uuid.Parse(hit.Id)
+			if err != nil {
+				log.Error("Error parsing uuid", "err", err)
+				continue
+			}
+			item, ok := gDataMap[outputId]
+			if !ok {
+				log.Error("Error retrieving gallery data", "output_id", outputId)
+				continue
+			}
+			generations = append(generations, item)
+		}
+		generationsUnsorted.Outputs = generations
+
+		if total != nil {
+			// uint to int
+			totalInt := int(*total)
+			generationsUnsorted.Total = &totalInt
+		}
+
+		// Get next cursor
+		generationsUnsorted.Next = qdrantRes.Next
+
+		// Return generations
+		render.Status(r, http.StatusOK)
+		render.JSON(w, r, generationsUnsorted)
+		return
+	}
+
+	// Otherwise, query postgres
+	var cursor *time.Time
+	if cursorStr := r.URL.Query().Get("cursor"); cursorStr != "" {
+		cursorTime, err := utils.ParseIsoTime(cursorStr)
+		if err != nil {
+			responses.ErrBadRequest(w, r, "cursor must be a valid iso time string", "")
+			return
+		}
+		cursor = &cursorTime
 	}
 
 	// Get generaions
