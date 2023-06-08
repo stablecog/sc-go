90f0813b3739f75dba25328cccdb9e6e5cb3df9b
Brandon Berhent <bbedward@gmail.com>
2023-06-08 22:59:45 +0000

    Count runes not bytes

--- utils/credits.go
+++ utils/credits.go
@@ -2,10 +2,11 @@ package utils
 
 import (
 	"math"
+	"unicode/utf8"
 
 	"github.com/stablecog/sc-go/shared"
 )
 
 func CalculateVoiceoverCredits(prompt string) int32 {
-	return int32(math.Ceil(shared.VOICEOVER_CREDIT_COST_PER_CHARACTER * float64(len(prompt))))
+	return int32(math.Ceil(shared.VOICEOVER_CREDIT_COST_PER_CHARACTER * float64(utf8.RuneCountInString(prompt))))
 }
--- utils/credits_test.go
+++ utils/credits_test.go
@@ -12,4 +12,5 @@ func TestCalculateVoiceoverCredits(t *testing.T) {
 	assert.Equal(t, int32(1), CalculateVoiceoverCredits("123456789012345678901234567890123456789012345678901234567"))
 	// 58 is 2
 	assert.Equal(t, int32(2), CalculateVoiceoverCredits("1234567890123456789012345678901234567890123456789012345678"))
+	assert.Equal(t, int32(6), CalculateVoiceoverCredits("We wanted the API to be as simple and easy to use as possible, and we think that we’ve achieved it. Developers can generate and upscale images with all of the same features as Stablecog’s web interface. There are additional APIs for retrieving the available settings for various models, querying images with a variety of filters, and more."))
 }
