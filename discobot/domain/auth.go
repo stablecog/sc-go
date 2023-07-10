cb261cac240c97576af8df4dcbb37c71a575bfe2
yekta <ngyekta@gmail.com>
2023-07-11 02:49:36 +0300

    Text change

--- discobot/domain/auth.go
+++ discobot/domain/auth.go
@@ -47,8 +47,8 @@ func (d *DiscoDomain) CheckAuthorization(s *discordgo.Session, i *discordgo.Inte
 		err = responses.InitialInteractionResponse(s,
 			i,
 			&responses.InteractionResponseOptions{
-				EmbedTitle:   "🚀 Sign in to get started",
-				EmbedContent: "Create a new Stablecog account or sign in to your existing one to get started.\n\n",
+				EmbedTitle:   "🚀 Sign in to start",
+				EmbedContent: "Create a Stablecog account or sign in to your existing one to get started.\n\n",
 				EmbedFooter:  "By signing in you agree to our Terms of Service and Privacy Policy.",
 				ActionRowOne: []*components.SCDiscordComponent{
 					components.NewLinkButton("Sign in", fmt.Sprintf("https://stablecog.com/connect/discord?%s", params.Encode()), "🔑"),
