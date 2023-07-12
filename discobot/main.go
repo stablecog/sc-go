fd55480bd681364fdc8d3a47ce28832be12ebff1
Brandon Berhent <bbedward@gmail.com>
2023-07-12 13:50:52 +0000

    Trivial text tweak

--- discobot/main.go
+++ discobot/main.go
@@ -244,7 +244,7 @@ func main() {
 				log.Error("Error creating dm channel", "err", err)
 				continue
 			}
-			s.ChannelMessageSendEmbed(dmChannel.ID, dresponses.NewEmbed(fmt.Sprintf("👋 Hi! <@%s>!", dmUser.Username), "I'm Stuart, the Stablecog bot. I'm here to provide you a suite of AI tools to use right here on Discord.\n\nI'm still a work on progress, but you can interact with me in any channel where I am present.", ""))
+			s.ChannelMessageSendEmbed(dmChannel.ID, dresponses.NewEmbed(fmt.Sprintf("👋 Hi, <@%s>!", dmUser.Username), "I'm Stuart, the Stablecog bot. I'm here to provide you a suite of AI tools to use right here on Discord.\n\nI'm still a work on progress, but you can interact with me in any channel where I am present.", ""))
 
 			// See if interaction exists in sync map too
 			i := loginInteractionMap.Get(authMsg.DiscordId)
