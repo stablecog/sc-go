3b5ab325f009edbbd595275a041140eac9f77f61
yekta <ngyekta@gmail.com>
2023-07-12 05:51:12 +0300

    Double space

--- discobot/responses/interactions.go
+++ discobot/responses/interactions.go
@@ -232,6 +232,6 @@ func ErrorResponseInitialValidation(s *discordgo.Session, i *discordgo.Interacti
 func InitialLoadingResponse(s *discordgo.Session, i *discordgo.InteractionCreate, privacy RESPONSE_PRIVACY) error {
 	return InitialInteractionResponse(s, i, &InteractionResponseOptions{
 		Privacy:    privacy,
-		EmbedTitle: "<a:loading:1128517995711447091> Working on it...",
+		EmbedTitle: "<a:loading:1128517995711447091>  Working on it...",
 	})
 }
