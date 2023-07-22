8da2e747d24f74ba5109f199d176784b087b70ed
yekta <ngyekta@gmail.com>
2023-07-22 13:21:21 +0300

    Add aspect ratio

--- discobot/interactions/generation.go
+++ discobot/interactions/generation.go
@@ -255,7 +255,7 @@ func (c *DiscordInteractionWrapper) NewImageCommand() *DiscordInteraction {
 
 				// Send the image
 				_, err = responses.InteractionEdit(s, i, &responses.InteractionResponseOptions{
-					Content:      utils.ToPtr(fmt.Sprintf("<@%s> **%s**\n```Model: %s```", discordUserId, prompt, shared.GetCache().GetGenerationModelNameFromID(*req.ModelId))),
+					Content:      utils.ToPtr(fmt.Sprintf("<@%s> **%s**```Model: %s\nAspect Ratio: %s```", discordUserId, prompt, shared.GetCache().GetGenerationModelNameFromID(*req.ModelId), aspectRatio.String())),
 					ImageURLs:    imageUrls,
 					ActionRowOne: actionRowOne,
 				},
