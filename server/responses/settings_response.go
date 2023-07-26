94de20597dcb3d173e8002613de660f36e6b4b2b
Brandon Berhent <bbedward@gmail.com>
2023-07-26 21:08:45 +0000

    Send mask and zoom out stuff

--- server/responses/settings_response.go
+++ server/responses/settings_response.go
@@ -35,6 +35,8 @@ type ImageGenerationSettingsResponse struct {
 	InferenceSteps int32     `json:"inference_steps"`
 	Seed           *int      `json:"seed,omitempty"`
 	InitImageURL   string    `json:"init_image_url,omitempty"`
+	MaskImageURL   string    `json:"mask_image_url,omitempty"`
+	ZoomOutScale   *float32  `json:"zoom_out_scale,omitempty"`
 	PromptStrength *float32  `json:"prompt_strength,omitempty"`
 }
 
