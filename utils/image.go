639979c43600371b999a4366da0bb8769fae0c98
Brandon Berhent <bbedward@gmail.com>
2023-07-28 14:14:05 +0000

    Revert

--- utils/image.go
+++ utils/image.go
@@ -4,6 +4,9 @@ import (
 	"image"
 	"image/color"
 	"image/draw"
+	"math"
+
+	"github.com/disintegration/imaging"
 )
 
 // Create mask and background image for zoom-out/expansion
@@ -11,13 +14,31 @@ func CreateExpandImageSet(img image.Image, scaleDownBy float64, blurRadiusFracti
 	width := img.Bounds().Dx()
 	height := img.Bounds().Dy()
 
-	// Create the mask
-	mask := image.NewRGBA(image.Rect(0, 0, width, height))
-	whiteRect := image.Rect(0, 0, width, height/2)
-	blackRect := image.Rect(0, height/2, width, height)
+	newWidth := int(float64(width) * scaleDownBy)
+	newHeight := int(float64(height) * scaleDownBy)
 
-	draw.Draw(mask, whiteRect, image.NewUniform(color.White), image.Point{}, draw.Src)
-	draw.Draw(mask, blackRect, image.NewUniform(color.Black), image.Point{}, draw.Src)
+	blurRadiusPixels := int(blurRadiusFraction * math.Min(float64(width), float64(height)))
 
-	return img, mask
+	imgScaled := imaging.Resize(img, newWidth, newHeight, imaging.Lanczos)
+
+	x0 := (width - newWidth) / 2
+	y0 := (height - newHeight) / 2
+
+	bg := image.NewRGBA(image.Rect(0, 0, width, height))
+	draw.Draw(bg, bg.Bounds(), image.NewUniform(color.White), image.Point{}, draw.Src)
+	draw.Draw(bg, imgScaled.Bounds().Add(image.Pt(x0, y0)), imgScaled, image.Point{}, draw.Src)
+
+	maskWidth := newWidth - 2*blurRadiusPixels
+	maskHeight := newHeight - 2*blurRadiusPixels
+
+	mask := image.NewRGBA(image.Rect(0, 0, maskWidth, maskHeight))
+	draw.Draw(mask, mask.Bounds(), image.NewUniform(color.Black), image.Point{}, draw.Src)
+
+	maskBg := imaging.New(width, height, color.NRGBA{0, 0, 0, 0})
+	draw.Draw(maskBg, maskBg.Bounds(), image.NewUniform(color.White), image.Point{}, draw.Src)
+
+	draw.Draw(maskBg, mask.Bounds().Add(image.Pt(x0+blurRadiusPixels, y0+blurRadiusPixels)), mask, image.Point{}, draw.Src)
+	maskBg = imaging.Blur(maskBg, float64(blurRadiusPixels))
+
+	return bg, maskBg
 }
