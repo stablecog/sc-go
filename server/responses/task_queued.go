a703511865afb47d54d2f185df5737c4dfae40f2
Brandon Berhent <bbedward@gmail.com>
2023-08-02 14:08:57 +0000

    Include was auto submitted on queued response

--- server/responses/task_queued.go
+++ server/responses/task_queued.go
@@ -5,4 +5,6 @@ type TaskQueuedResponse struct {
 	ID               string `json:"id"`
 	UIId             string `json:"ui_id,omitempty"`
 	RemainingCredits int    `json:"total_remaining_credits"`
+	WasAutoSubmitted *bool  `json:"was_auto_submitted,omitempty"`
+	IsPublic         *bool  `json:"is_public,omitempty"`
 }
