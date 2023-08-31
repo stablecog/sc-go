8b4b03bb9e9ff9b73bc2e42e9ae0d8cbc7e67b80
yekta <ngyekta@gmail.com>
2023-08-31 04:37:27 +0300

    Message tweaks

--- discobot/main.go
+++ discobot/main.go
@@ -287,7 +287,7 @@ func main() {
 				log.Error("Error creating dm channel", "err", err)
 				continue
 			}
-			s.ChannelMessageSendEmbed(dmChannel.ID, dresponses.NewEmbed(fmt.Sprintf("👋 Hi, %s!", dmUser.Username), "I'm Stuart, the Stablecog bot. I'm here to provide you a suite of AI tools.\n\nTry one of the following commands:\n\n/image\n/voiceover", ""))
+			s.ChannelMessageSendEmbed(dmChannel.ID, dresponses.NewEmbed(fmt.Sprintf("👋 Hi, @%s!", dmUser.Username), "I'm Stuart, the Stablecog bot. I'm here to provide you a suite of AI tools.\n\nTry one of the following commands:\n\n/imagine\n/speak", ""))
 
 			// See if interaction exists in sync map too
 			i := loginInteractionMap.Get(authMsg.DiscordId)
@@ -295,7 +295,7 @@ func main() {
 				// Update interaction
 				dresponses.InteractionEdit(i.Session, i.Interaction, &dresponses.InteractionResponseOptions{
 					EmbedTitle:   "✅ Authenticated!",
-					EmbedContent: "You have successfully authenticated with Stablecog! You can now use my AI tools.\n\nTry one of the following commands:\n\n/image\n/voiceover",
+					EmbedContent: "You have successfully authenticated with Stablecog! You can now use various commands.\n\nTry one of the following commands:\n\n/imagine\n/speak",
 				})
 
 				// Remove from sync map
