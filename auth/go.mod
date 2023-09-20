3ab6648277c04bc6df51b15b75df9e582a425db0
Brandon Berhent <bbedward@gmail.com>
2023-09-20 17:29:48 +0000

    Add stub auth service

---
+++ auth/go.mod
@@ -0,0 +1,31 @@
+module github.com/stablecog/sc-go/auth
+
+go 1.20
+
+require github.com/go-oauth2/oauth2/v4 v4.5.2
+
+require (
+	github.com/json-iterator/go v1.1.7 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
+	github.com/modern-go/reflect2 v0.0.0-20180701023420-4b7aa43c6742 // indirect
+	gopkg.in/oauth2.v3 v3.10.1 // indirect
+)
+
+require (
+	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
+	github.com/jackc/pgconn v1.10.1 // indirect
+	github.com/jackc/pgio v1.0.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgproto3/v2 v2.2.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20200714003250-2b9c44734f2b // indirect
+	github.com/jackc/pgtype v1.9.1 // indirect
+	github.com/jackc/pgx/v4 v4.14.1 // indirect
+	github.com/jackc/puddle v1.2.0 // indirect
+	github.com/jmoiron/sqlx v1.3.4 // indirect
+	github.com/vgarvardt/go-oauth2-pg v1.3.0
+	github.com/vgarvardt/go-oauth2-pg/v4 v4.4.3 // indirect
+	github.com/vgarvardt/go-pg-adapter v1.0.0 // indirect
+	github.com/vgarvardt/pgx-helpers/v4 v4.0.0-20200225100150-876aee3d1a22 // indirect
+	golang.org/x/crypto v0.0.0-20220214200702-86341886e292 // indirect
+	golang.org/x/text v0.3.7 // indirect
+)
---
+++ auth/main.go
@@ -0,0 +1,142 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httputil"
+	"os"
+	"time"
+
+	"github.com/jackc/pgx/v4"
+	"github.com/joho/godotenv"
+	"github.com/stablecog/sc-go/database"
+	"github.com/stablecog/sc-go/log"
+	pg "github.com/vgarvardt/go-oauth2-pg/v4"
+	"github.com/vgarvardt/go-pg-adapter/pgx4adapter"
+
+	"github.com/go-oauth2/oauth2/v4/errors"
+	"github.com/go-oauth2/oauth2/v4/manage"
+	"github.com/go-oauth2/oauth2/v4/server"
+)
+
+func main() {
+	// Load .env
+	err := godotenv.Load("../.env")
+	if err != nil {
+		log.Warn("Error loading .env file (this is fine)", "err", err)
+	}
+
+	dbconn, err := database.GetSqlDbConn(false)
+	if err != nil {
+		log.Fatal("Failed to connect to database", "err", err)
+		os.Exit(1)
+	}
+
+	pgxConn, _ := pgx.Connect(context.TODO(), dbconn.DSN())
+
+	manager := manage.NewDefaultManager()
+
+	// use PostgreSQL token store with pgx.Connection adapter
+	adapter := pgx4adapter.NewConn(pgxConn)
+	tokenStore, _ := pg.NewTokenStore(adapter, pg.WithTokenStoreGCInterval(time.Minute))
+	defer tokenStore.Close()
+
+	clientStore, _ := pg.NewClientStore(adapter)
+
+	manager.MapTokenStorage(tokenStore)
+	manager.MapClientStorage(clientStore)
+
+	cfg := server.NewConfig()
+	cfg.ForcePKCE = true
+	srv := server.NewServer(cfg, manager)
+
+	srv.SetUserAuthorizationHandler(userAuthorizeHandler)
+
+	srv.SetInternalErrorHandler(func(err error) (re *errors.Response) {
+		log.Infof("Internal Error: %v", err.Error())
+		return
+	})
+
+	srv.SetResponseErrorHandler(func(re *errors.Response) {
+		log.Infof("Response Error: %v", re.Error.Error())
+	})
+
+	// Health check endpoint
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	})
+
+	http.HandleFunc("/oauth/authorize", func(w http.ResponseWriter, r *http.Request) {
+		err := srv.HandleAuthorizeRequest(w, r)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+		}
+	})
+
+	http.HandleFunc("/oauth/token", func(w http.ResponseWriter, r *http.Request) {
+		srv.HandleTokenRequest(w, r)
+	})
+
+	portvar := 9096
+
+	log.Infof("Server is running at %d port.\n", portvar)
+	log.Infof("Point your OAuth client Auth endpoint to %s:%d%s", "http://localhost", portvar, "/oauth/authorize")
+	log.Infof("Point your OAuth client Token endpoint to %s:%d%s", "http://localhost", portvar, "/oauth/token")
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", portvar), nil))
+}
+
+func dumpRequest(writer io.Writer, header string, r *http.Request) error {
+	data, err := httputil.DumpRequest(r, true)
+	if err != nil {
+		return err
+	}
+	writer.Write([]byte("\n" + header + ": \n"))
+	writer.Write(data)
+	return nil
+}
+
+func userAuthorizeHandler(w http.ResponseWriter, r *http.Request) (userID string, err error) {
+	_ = dumpRequest(os.Stdout, "userAuthorizeHandler", r) // Ignore the error
+	// store, err := session.Start(r.Context(), w, r)
+	// if err != nil {
+	// 	return
+	// }
+
+	// uid, ok := store.Get("LoggedInUserID")
+	// if !ok {
+	// 	if r.Form == nil {
+	// 		r.ParseForm()
+	// 	}
+
+	// 	store.Set("ReturnUri", r.Form)
+	// 	store.Save()
+
+	// 	w.Header().Set("Location", "/login")
+	// 	w.WriteHeader(http.StatusFound)
+	// 	return
+	// }
+
+	// userID = uid.(string)
+	// store.Delete("LoggedInUserID")
+	// store.Save()
+	return
+}
+
+func authHandler(w http.ResponseWriter, r *http.Request) {
+	_ = dumpRequest(os.Stdout, "auth", r) // Ignore the error
+	// store, err := session.Start(nil, w, r)
+	// if err != nil {
+	// 	http.Error(w, err.Error(), http.StatusInternalServerError)
+	// 	return
+	// }
+
+	// if _, ok := store.Get("LoggedInUserID"); !ok {
+	// 	w.Header().Set("Location", "/login")
+	// 	w.WriteHeader(http.StatusFound)
+	// 	return
+	// }
+
+	// outputHTML(w, r, "static/auth.html")
+}
