80aff960f1a63ad2faaf2331ddac82c891385526
Brandon Berhent <bbedward@gmail.com>
2024-03-13 11:57:42 +0000

    Remove order

--- database/repository/generation_queries.go
+++ database/repository/generation_queries.go
@@ -915,10 +915,6 @@ func (r *Repository) QueryGenerationsAdmin(per_page int, cursor *time.Time, call
 	}
 	queryG = r.ApplyUserGenerationsFilters(queryG, filters, false)
 	queryG = queryG.Where(func(s *sql.Selector) {
-		got := sql.Table(generationoutput.Table).As("t1")
-		if cursor != nil {
-			s.Where(sql.LT(got.C(generationoutput.FieldCreatedAt), *cursor))
-		}
 		s.Where(sql.IsNull("deleted_at"))
 	})
 	err := queryG.Limit(per_page+1).Modify(func(s *sql.Selector) {
