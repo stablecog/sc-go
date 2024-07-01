db15f59f6a63ced329763f6e648ce7286b31308a
Brandon Berhent <bbedward@gmail.com>
2024-07-01 13:45:16 +0000

    Re-do group by

--- database/repository/gallery_queries.go
+++ database/repository/gallery_queries.go
@@ -196,6 +196,7 @@ func (r *Repository) RetrieveMostRecentGalleryDataV2(filters *requests.QueryGene
 			got.C(generationoutput.FieldImagePath), got.C(generationoutput.FieldUpscaledImagePath),
 			got.C(generationoutput.FieldLikeCount), got.C(generationoutput.FieldDeletedAt),
 			got.C(generationoutput.FieldIsFavorited), got.C(generationoutput.FieldIsPublic),
+			got.C(generationoutput.FieldCreatedAt), got.C(generationoutput.FieldUpdatedAt),
 			ut.C(user.FieldUsername), ut.C(user.FieldID),
 			sql.Table("like_subquery").C("like_count_trending"))
 
