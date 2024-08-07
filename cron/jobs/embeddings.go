fe8f9c38f84654c9797d0f87863bd1519e325c83
yekta <ngyekta@gmail.com>
2024-08-07 03:38:46 +0300

    Pretty print

--- cron/jobs/embeddings.go
+++ cron/jobs/embeddings.go
@@ -1,5 +1,7 @@
 package jobs
 
+import "encoding/json"
+
 const GET_EMBEDDINGS_JOB_NAME = "GET_EMBEDDINGS_JOB"
 
 func (j *JobRunner) GetEmbeddingsAndUpdateQdrant(log Logger) error {
@@ -9,6 +11,20 @@ func (j *JobRunner) GetEmbeddingsAndUpdateQdrant(log Logger) error {
 		log.Errorf("Error getting outputs with no embeddings: %v", err)
 		return err
 	}
-	log.Infof("Found outputs with no embeddings", outputs[0])
+
+	if len(outputs) > 0 {
+		// Convert the first output to JSON
+		jsonData, err := json.MarshalIndent(outputs[0], "", "    ")
+		if err != nil {
+			log.Errorf("Error marshaling output to JSON: %v", err)
+			return err
+		}
+
+		// Print the JSON-formatted output
+		log.Infof("Found outputs with no embeddings: %s", string(jsonData))
+	} else {
+		log.Infof("No outputs found with no embeddings")
+	}
+
 	return nil
 }
